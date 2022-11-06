a1557bdf5b87561baad864a4de1f86e9a99e1fdb
Zehra Karakaya <zehrakarakaya999@gmail.com>
2022-11-07 02:29:09 +0300

    küçük ekler

--- app/src/main/java/com/example/myapplication/Sınıf.kt
+++ app/src/main/java/com/example/myapplication/Sınıf.kt
@@ -3,9 +3,11 @@ package com.example.myapplication
 class Sınıf {
     var AdSoyad=""
     var Sifre=""
+    var mail=""
 
-    constructor(AdSoyad:String,Sifre:String){
+    constructor(AdSoyad:String,Sifre:String,mail:String){
         this.AdSoyad=AdSoyad
         this.Sifre=Sifre
+        this.mail=mail
     }
 }
