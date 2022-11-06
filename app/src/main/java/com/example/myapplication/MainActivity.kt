86da61c4057523624589ea21b188059acfd9b683
Zehra Karakaya <zehrakarakaya999@gmail.com>
2022-11-06 15:19:04 +0300

    birkaç ek

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+    fun Giris(view: View){
+
+    }
+    fun Kaydol(view: View){
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Sınıf.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+class Sınıf {
+    var AdSoyad=""
+    var Sifre=""
+
+    constructor(AdSoyad:String,Sifre:String){
+        this.AdSoyad=AdSoyad
+        this.Sifre=Sifre
+    }
+}
