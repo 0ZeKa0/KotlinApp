35ee817585e2a0464707a03b9fa67cda3a5c0dfc
Zehra Karakaya <zehrakarakaya999@gmail.com>
2022-11-15 04:17:16 +0300

    Şifremi Unuttum Kısmı için xml belgesi düzenlendi.

--- app/src/main/java/com/example/myapplication/MainActivity4.kt
+++ app/src/main/java/com/example/myapplication/MainActivity4.kt
@@ -2,10 +2,15 @@ package com.example.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main4)
+    }
+
+    fun Gonder(view: View){
+
     }
 }
