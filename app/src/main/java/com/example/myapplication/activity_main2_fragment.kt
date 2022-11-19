62cd31acfa12b9d895c2441f5e7070fb554e5f4e
Zehra Karakaya <zehrakarakaya999@gmail.com>
2022-11-20 00:41:14 +0300

    working on fragment3

--- app/src/main/java/com/example/myapplication/activity_main2_fragment.kt
+++ app/src/main/java/com/example/myapplication/activity_main2_fragment.kt
@@ -1,10 +1,14 @@
 package com.example.myapplication
 
+import android.R.attr.data
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil.setContentView
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.ActivityMain2Binding
 import com.example.myapplication.databinding.FragmentActivityMain2FragmentBinding
 
 
@@ -17,14 +21,23 @@ class activity_main2_fragment : Fragment() {
         super.onCreate(savedInstanceState)
 
     }
-    fun ayarla(view: View){
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_activity_main2_fragment, container, false)
-    }}
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fun ayarla(view: View){
+            binding.ayarla.setOnClickListener{
+                val intent = Intent(activity,MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+}
 
