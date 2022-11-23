3af94b82a32791526302c5d394f4430e9fe0b285
Zehra Karakaya <zehrakarakaya999@gmail.com>
2022-11-23 19:20:00 +0300

    recycler

--- app/src/main/java/com/example/myapplication/RecycleAdapter.kt
+++ app/src/main/java/com/example/myapplication/RecycleAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.view.menu.ActionMenuItemView
@@ -7,7 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Recycler
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
-class RecycleAdapter : RecyclerView.Adapter<RecycleAdapter.GorunumOlusturucu>() {
+class RecycleAdapter(val calisma_suresi: ArrayList<String>,val Ish: ArrayList<String>) : RecyclerView.Adapter<RecycleAdapter.GorunumOlusturucu>() {
     override fun getItemCount(): Int {
         TODO("Not yet implemented")
     }
@@ -18,8 +19,11 @@ class RecycleAdapter : RecyclerView.Adapter<RecycleAdapter.GorunumOlusturucu>() 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GorunumOlusturucu {
-        TODO("Not yet implemented")
+        val inflater=LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.fragment_2,parent,false)
+        return GorunumOlusturucu(view)
     }
+
 
     override fun onBindViewHolder(holder: GorunumOlusturucu, position: Int) {
         TODO("Not yet implemented")
