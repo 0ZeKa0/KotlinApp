6f7bfb333621304ea780f386e9744ffe977adea4
Zehra Karakaya <zehrakarakaya999@gmail.com>
2022-12-01 21:50:03 +0300

    Çıkış elemanı eklendi

--- app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -11,10 +12,13 @@ import android.view.MenuItem
 import android.view.View
 import com.example.myapplication.ui.main.SectionsPagerAdapter
 import com.example.myapplication.databinding.ActivityMain2Binding
+import com.google.firebase.auth.FirebaseAuth
+import java.util.zip.Inflater
 
 class MainActivity2 : AppCompatActivity() {
 
     private lateinit var binding: ActivityMain2Binding
+    private lateinit var auth:FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +37,22 @@ class MainActivity2 : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater=menuInflater
+        menuInflater.inflate(R.menu.secenekler_menusu,menu)
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        auth.signOut()
+        val intent = Intent(applicationContext,MainActivity::class.java)
+        startActivity(intent)
+        finish()
+        return super.onOptionsItemSelected(item)
     }
 
     fun Ekle(view: View){
