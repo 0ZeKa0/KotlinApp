e3285f1c6fd6343ca4c073a2c19a378093f72bdf
Konstantinopolis <zehrakarakaya999@gmail.com>
2022-12-23 14:29:47 +0300

    fixed

--- app/src/main/java/com/example/myapplication/view/RecyclerAdapterFlashcards.kt
+++ app/src/main/java/com/example/myapplication/view/RecyclerAdapterFlashcards.kt
@@ -8,14 +8,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
 
 class RecyclerAdapterFlashcards(val description: ArrayList<String>,val word: ArrayList<String>) : RecyclerView.Adapter<RecyclerAdapterFlashcards.GorunumOlusturucu2>() {
-    class GorunumOlusturucu2(itemView: View) {
+
+    class GorunumOlusturucu2(itemView: View) :RecyclerView.ViewHolder(itemView){
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GorunumOlusturucu2 {
         val inflater=LayoutInflater.from(parent.context)
         val view = inflater.inflate(R.layout.fragment_flashcards,parent,false)
-        return RecyclerAdapterFlashcards.GorunumOlusturucu2(view)
+        return GorunumOlusturucu2(view)
     }
 
     override fun onBindViewHolder(holder: GorunumOlusturucu2, position: Int) {
@@ -23,7 +24,7 @@ class RecyclerAdapterFlashcards(val description: ArrayList<String>,val word: Arr
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return word.size
     }
 
 }
